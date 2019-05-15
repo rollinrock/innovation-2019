4729e509146c9dcf1e2364f2f20ce7a8df7131fc
cao jing <cj@caodeMacBook-Pro.local>
2019-05-15 17:55:46 +0800

    vue & vue-amap trial code for demo

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h4>{{title}}</h4>
+    </header>
+    <body>
+      <div class="container">
+        <div class="row">
+          <div class="col-3">
+            <district-explorer></district-explorer>
+          </div>
+          <div class="search-map-container col-9">
+            <search-map></search-map>
+          </div>
+        </div>
+      </div>
+    </body>
+    <footer>
+      <p> &copy;中国联通金华分公司 联系邮箱：<a href="mailto:caoj30@chinaunicom.cn">caoj30@chinaunicom.cn</a></p>
+    </footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import SearchMap from './components/SearchMap.vue';
+import DistrictExplorer from './components/DistrictExplorer.vue';
 
 export default {
   name: 'app',
+  data: function() {
+    return {
+      title: '中国联通创新2019'
+    }
+  },
   components: {
-    HelloWorld
+    SearchMap, DistrictExplorer
   }
 }
 </script>
@@ -23,6 +47,23 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
+
+header, footer {
+  height: 100px;
+}
+
+div {
+  /* border: 1px solid gray; */
+}
+
+.search-map-container {
+  height: 600px;
+}
+
+.h600 {
+  height: 600px;
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueAMap from "vue-amap";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueAMap);
+VueAMap.initAMapApiLoader({
+  key: 'dae1306a042d8456178b112d2ec3ea1d',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+  uiVersion: '1.0.11', // 版本号
+  // 默认高德 sdk 版本为 1.4.14
+  v: '1.4.14'
+})
 
 new Vue({
   render: h => h(App),
